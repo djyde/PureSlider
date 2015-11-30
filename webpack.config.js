b78a57380e24e4d7dc9e3e73e37d5f5efa9464e1
Randy <randypriv@gmail.com>
2015-11-30 12:19:31 +0800

    structure

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: './src/pureslider.js',
+  output: {
+    filename: 'pureslider.dist.js',
+    path: './dist',
+    library: 'PureSlider',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel?presets[]=es2015'
+      }
+    ]
+  }
+}
